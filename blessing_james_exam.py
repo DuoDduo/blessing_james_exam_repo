677f1a7233dafa10d7469364ee56544bf1239f20
Blessing <pelubs2956@gmail.com>
2025-09-04 15:28:20 +0800

    first commit

---
+++ blessing_james_exam.py
@@ -0,0 +1,116 @@
+# Question 1: Calculator
+
+# function to add two numbers
+def add(a,b):
+    return a + b
+# function to subtract two numbers
+def subtract(a,b):
+    return a - b
+# function to multiply two numbers
+def multiply(a,b):
+    return a * b
+# function to divide two numbers
+def divide(a,b):
+   try:
+        if b== 0 or a==0:
+            print("Error: input cannot be a negative number")
+        else:    
+            return a / b
+   except ZeroDivisionError as e:
+         print("Error: ", e)
+    
+
+# Main program
+def main():
+    while True:
+        try:
+           #Calculator menu
+            print("\nWelcome to Blessing's Calculator")
+            print("1. Add (+)")
+            print("2. Subtract (-)")
+            print("3. Multiply (*)")
+            print("4. Divide (/)")
+            print("5. Exit")
+
+            choice = int(input("Enter between (1-5) to select your desired calculator operation: "))
+            # Validate choice using range
+            if choice not in range(1, 6):
+                print("Invalid choice! Please choose a number between 1 and 5.")
+                continue
+            if choice == 5:
+                print("Exiting calculator...")
+                break  
+            # Handle user inputs
+            if choice in range(1,6):
+                a = float(input("Enter first number: "))
+                b = float(input("Enter second number: "))
+                    # Display calculator menu
+                if choice == 1:
+                    result = add(a, b)
+                elif choice == 2:
+                    result = subtract(a, b)
+                elif choice == 3:
+                    result = multiply(a, b)
+                elif choice == 4:
+                    result = divide(a, b)
+               # Exit condition
+            
+
+            print(f"Result =  {result}")
+            
+            # Ask if user wants another calculation
+            again = input("Do you want to perform another operation? (yes/no): ").strip().lower()
+            if again != 'yes':
+                print("Thanks for using the calculator. Goodbye!")
+                break
+
+        except ValueError:
+            print("Error: Please enter a valid number!")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+
+# Run the calculator
+if __name__ == "__main__":
+    main()    
+
+
+
+
+
+
+
+# Question2
+while True:
+    user_input = input("\nEnter a number (or type 'exit' to quit): ")
+    if user_input == "exit":
+        print("Goodbye!\n")
+        break   # break out of loop
+    
+    num = int(user_input)   # convert to integer
+    
+    if num % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+
+
+
+
+
+
+
+#  Question 3 
+while True:
+    try:
+        age = input("\nEnter your age (or type exit to quit): ")
+
+        if age == "exit":
+            print("\nGoodbye!")
+            break
+        
+        elif int(age) >= 18:
+            print("You can vote")
+        else:
+            print("You cannot vote")
+    except ValueError:
+        print("Invalid input, Please enter a number")
---
+++ readme.md
@@ -0,0 +1,30 @@
+## Blessing James Examination(AI-ENGINEERING)
+
+## Question 1 
+This involves writing a programme for a basic calculator function with continuous use
+The function covers: addition, subtraction, multiplication and division function.
+
+The program repeatedly allows the user to perform operations until they choose to exit.
+
+Program flow: 
+- Display welcome message and calculator menu to the user
+- Ask them to choose an operation: addition, subtraction, multiplication, or division.
+- Ask the user to enter two numbers.
+
+I used functions to define each operation.
+I used try-except to handle invalid input and division by zero.
+I used control flow (if-elif-else) to validate selection of correct calculator operation.
+I used a while loop to keep running until the user chooses to exit.
+
+## Question 2
+This involves completing a partially writtten code
+I completed the missing parts of the program so that it keeps asking the user for a number and tells whether it is even or odd.
+
+The code involving using input, int conversion, if-else control flow, and print output now works  perfectly in the programme
+The while loop now runs until the user types 'exit'.
+
+## Question 3
+
+This involves debugging an incorrect code
+- I identified and fix the errors which is validating the user input by typecasting to int
+-I refactored the try except block to catch invalid input
